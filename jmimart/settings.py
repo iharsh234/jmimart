a46ab4838ad1a667be0c590ecedfad3ff8968d28
Zishan Ahmad <zishanahmad@outlook.com>
2015-07-13 22:36:00 +0530

    changed the database

--- jmimart/settings.py
+++ jmimart/settings.py
@@ -78,14 +78,11 @@ WSGI_APPLICATION = 'jmimart.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'jmimart',
-        'USER': 'zishan',
-        'PASSWORD': 'zishan',
-        'HOST': 'localhost',
-        'POST': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
 
 
 
